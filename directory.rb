3db668e1522383e2edefb56b56bef3e94fe9103a
Mike <mjwalshe@gmail.com>
2019-08-14 22:18:13 +0100

    Complete exercises 1–3

--- directory.rb
+++ directory.rb
@@ -16,8 +16,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, index|
+    if student[:name][0].downcase == 'p' && student[:name].length <= 12
+      puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
